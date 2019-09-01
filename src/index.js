1e8c0171665f0cd46c38d5fbe0e3cedbf81b8223
Marlon Francois <marlonrorodriguez2b@gail.com>
2019-09-01 07:37:56 -0500

    none
    ok

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 const express =require('express');
 const app = express();
 const path = require ('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 
 
 
 //configuracion
+
 app.set('port', 3000);
 app.set('views', path.join(__dirname,'views'));
 app.engine('html', require ('ejs').renderFile);
 app.set('view engine','ejs');
 
+  //COnfiguracion body-parser
+  
+
+  app.use(bodyParser.urlencoded({extended: false}))
+  app.use(bodyParser.json())
+ 
+
 
 
 //middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 //routes
@@ -23,8 +36,8 @@ app.use (require ('./routes/index'));
 app.use(express.static(path.join(__dirname, 'Public')))
 
 
-
 //escuchando servidor 
+
 app.listen(app.get('port'), () => {
     console.log('Server on port' , app.get('port'));
 } );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,30 +1,46 @@
 const express = require('express');
 const router = express.Router();
-
+const bodyParser = require('body-parser');
 const Proceso =require('../Proceso/to2alv');
+const app= express();
 
-//router.get('/', Proceso.to2alv);
+
+
+
+
+/*app.get('/calculo', function(req, res){
+   console.log("Proceso.to2alv");
+  });*/
+
+  router.post('/calculo',  (req, res) =>  {
+      console.log(req.body);
+      res.render('calc.ejs',{Titulo:'equisde', ip: 'ip'});
+      router.post('/calculo', Proceso.to2alv);
+   // res.send("Direccion ip:" + req.body.ip + "Mascara" + req.body.mask); 
+    
+ 
+});
+
+
+/*router.get('/', Proceso.to2alv);
 
 router.post('/calculador', function(req,res, next){
-    res.redirect('/calculadora');
+  res.redirect('views/calculo.html',);
     var id= req.body.id;
-}); //Proceso.calculador);
+}); (Proceso.calculador);
 
-//rutas
+//rutas*/
+router.get('/calculadora',(req, res) => {
+    res.render('calc.html', {Titulo: 'Calculadora'}); 
+ 
+})
+
 router.get('/',(req, res) => {
     res.render('index.html', {Titulo: 'Inicio'}); 
  
 })
 
-router.get('/calculo',(req, res) => {
-    res.render('calculo.html', {Titulo: 'Aqui se hacen los calculos'}); 
- 
-})
 
-router.get('/calculadora',(req, res) => {
-    res.render('calc.html', {Titulo: 'Calculadora'}); 
- 
-})
 
 router.get('/memes',(req, res) => {
     res.render('memes.html', {Titulo: 'Memes'}); 
